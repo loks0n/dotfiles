84937273646d9072f2562e6c2f0dc8dc77e120f3
loks0n <22452787+loks0n@users.noreply.github.com>
2025-11-30 15:53:07 +0000

    feat: aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,24 @@
 set -g fish_greeting
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+# aliases
+alias lg="lazygit log"
+alias k="kubectl"
+
+# git aliases
+alias gd="git diff --output-indicator-new=' ' --output-indicator-old=' '"
+
+alias ga="git add"
+alias gap="git add --patch"
+alias gc="git commit"
+
+alias gs="git switch"
+alias gr="git restore"
+
+alias gp="git push"
+alias gu="git pull"
+
+alias gl='git log --graph --all --pretty=format:"%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n"'
+alias gb="git branch"
+
+alias gi="git init"
+alias gcl="git clone"
